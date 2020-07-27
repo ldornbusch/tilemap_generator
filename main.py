12b4e1580daa1836d1c6720ed1bffe355b6c6d6e
Lutz Dornbusch <ldornbusch@yogasurftech.de>
2020-07-28 01:29:39 +0200

    optimize tileset_map creation by keeping tiles together and only remove empty columns or rows

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import os.path
 import math
 from PIL import Image
+from PIL import ImageDraw
 
-# todo: better tile sort for easier reuse
+# progress: better tile sort for easier reuse
 # todo: command line interface
 # todo: output time needed
 
 img_path = r"tom_c16_map.gif"
 # img_path = r"850_gamemap2.png"
-# img_path = r"894_gamemap3.png"
+# img_path = r"894_gamemap7.png"
 # img_path = r"894_gamemap5.png"
+# img_path = r"894_gamemap3.png"
+# img_path = r"894_gamemap14.png"
 tiles = (8, 8)
 color = "ff00ff"
 
@@ -24,19 +27,49 @@ def extract_tiles(str_img_path, tile_size, transparent_color="ff00ff"):
     print(img.format, img.size, img.mode)
     print("image size: " + str(img.size))
     print("tile size:  " + str(tile_size))
-    print("atlas  size:" + str((img.width // tile_size[0], img.height // tile_size[1])))
+    print("map  size:" + str((img.width // tile_size[0], img.height // tile_size[1])))
 
-    all_tile_img, tile_atlas = deduplicate_tiles(img, tile_size)
+    tileset_size = [img.width // tile_size[0], img.height // tile_size[1]]
 
-    catalog_size, tile_set = assemble_tileset(all_tile_img, img, tile_size)
+    all_tile_img, tile_atlas, expanded_atlas, tileset_map = deduplicate_tiles(img, tile_size)
+
+    del_lines = []
+    for y in reversed(range(tileset_size[1])):
+        accu = 0
+        for x in range(tileset_size[0]):
+            accu += tileset_map[y*tileset_size[0]+x]
+        if accu == 0:
+            del_lines.append(y)
+
+    del_cols = []
+    for x in reversed(range(tileset_size[0])):
+        accu = 0
+        for y in reversed(range(tileset_size[1])):
+            accu += tileset_map[y*tileset_size[0]+x]
+        if accu == 0:
+            del_cols.append(x)
+
+    for y in del_lines:
+        for x in reversed(range(tileset_size[0])):
+            tileset_map.pop(y*tileset_size[0]+x)
+        tileset_size[1] = tileset_size[1] - 1
+
+    for x in del_cols:
+        for y in reversed(range(tileset_size[1])):
+            tileset_map.pop(y*tileset_size[0]+x)
+        tileset_size[0] = tileset_size[0] - 1
+    print("optimized tileset_size:"+str(tileset_size))
+
+    catalog_size, tile_set, tile_mapping = assemble_tileset(all_tile_img, img, tile_size, tileset_map, tileset_size)
 
     catalog_filename = os.path.join(str_path, str_subject + "_catalog" + str_suffix)
+    # tile_set.save(catalog_filename, palette=img.getpalette())
     tile_set.save(catalog_filename, palette=img.getpalette())
     print("Number of unique tiles:" + str(len(all_tile_img)))
 
     tiles_per_column = img.width // tile_size[0]
     tiles_per_row = img.height // tile_size[1]
-    tilemap_data = generate_tilemap_data(tile_atlas, (tiles_per_row,tiles_per_column))
+    tilemap_data = generate_tilemap_data(tile_atlas, tile_mapping, (tiles_per_row, tiles_per_column))
 
     tiled_file = """<?xml version="1.0" encoding="UTF-8"?>
 <map version="1.2" tiledversion="1.3.3" orientation="orthogonal" renderorder="right-down" 
@@ -58,11 +91,11 @@ infinite="0" nextlayerid="2" nextobjectid="1">
        "tileheight": tile_size[1],
        "tileset_name": str_subject,
        "tilecount": len(all_tile_img),
-       "columns": catalog_size,
+       "columns": catalog_size[0],
        "catalog_file": catalog_filename,
        "transparent": transparent_color,
-       "catalog_width": catalog_size * tile_size[0],
-       "catalog_height": catalog_size * tile_size[1],
+       "catalog_width": catalog_size[0] * tile_size[0],
+       "catalog_height": catalog_size[1] * tile_size[1],
        "atlas_data": tilemap_data}
     map_filename = os.path.join(str_path, str_subject + ".tmx")
     f = open(map_filename, "w")
@@ -70,36 +103,40 @@ infinite="0" nextlayerid="2" nextobjectid="1">
     f.close()
 
 
-def generate_tilemap_data(tile_atlas, tile_count):
+def generate_tilemap_data(tile_atlas, tile_mapping, tile_count):
     atlas_data = ""
     for row in range(tile_count[0]):
         accu_row = ""
         for col in range(tile_count[1]):
             index = row * tile_count[1] + col
-            accu_row += format("%d, " % tile_atlas[index])
+            accu_row += format("%d, " % (tile_mapping[tile_atlas[index]]+1))
         atlas_data += accu_row + "\n"
     atlas_data = atlas_data[:-3]
     return atlas_data
 
 
-def assemble_tileset(all_tile_img, img, tile_size):
-    catalog_size = math.ceil(math.sqrt(len(all_tile_img)))
-    tile_catalog = Image.new(img.mode, (catalog_size * tile_size[0], catalog_size * tile_size[1]))
-    index = 0
-    for tile_img in all_tile_img:
-        tmp = tile_img.copy()
-        tile_catalog.paste(tmp,
-                           ((index % catalog_size) * tile_size[0],
-                            (index // catalog_size) * tile_size[1]))
-        index += 1
-    return catalog_size, tile_catalog
+def assemble_tileset(all_tile_img, img, tile_size, tileset_map, tileset_size):
+    tile_mapping = {}
+    tile_catalog = Image.new(img.mode, (tileset_size[0] * tile_size[0], tileset_size[1] * tile_size[1]))
+    for y in range(tileset_size[1]):
+        for x in range(tileset_size[0]):
+            img_id = tileset_map[y * tileset_size[0] + x]
+            if img_id != 0:
+                tile_mapping[img_id] = y * tileset_size[0] + x
+                tmp = all_tile_img[img_id - 1].copy()
+                tile_catalog.paste(tmp, (x * tile_size[0], y * tile_size[1]))
+
+    return tileset_size, tile_catalog, tile_mapping
 
 
 def deduplicate_tiles(img, tile_size):
+    img_atlas = img.copy()  # the img_atlas is nearly empty and shows every tile only once
+    draw = ImageDraw.Draw(img_atlas)
     all_tiles = {}  # hashmap of (str(tile) -> index)
     all_tiles_imgs = []  # list of tiles
     tile_atlas = []  # indexes to tiles
-    imgdata = img.getdata()
+    img_tile_atlas = []  # indexes to tiles in secondary image atlas (for better reordering)
+    # imgdata = img.getdata()
     for ty in range(img.height // tile_size[1]):
         for tx in range(img.width // tile_size[0]):
             tile_data = []
@@ -117,8 +154,16 @@ def deduplicate_tiles(img, tile_size):
                                                 tx * tile_size[0] + tile_size[0],
                                                 ty * tile_size[1] + tile_size[1])))
                 all_tiles[str_tile_data] = len(all_tiles_imgs)
+                img_tile_atlas.append(len(all_tiles_imgs))
+            else:
+                # remove the tile ()
+                draw.rectangle((tx * tile_size[0],
+                                ty * tile_size[1],
+                                tx * tile_size[0] + tile_size[0],
+                                ty * tile_size[1] + tile_size[1]), fill=(0xff, 0, 0xff, 0))
+                img_tile_atlas.append(0)
             tile_atlas.append(all_tiles[str_tile_data])
-    return all_tiles_imgs, tile_atlas
+    return all_tiles_imgs, tile_atlas, img_atlas, img_tile_atlas
 
 
 extract_tiles(img_path, tiles, color)
